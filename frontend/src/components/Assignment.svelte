f4966af0f847dac0353b83e80eca6bf03d5cdc5e
tikibeni <benjamin.blinnikka@hotmail.com>
2024-01-02 12:00:14 +0200

    Refactor singular assignments into their own component
    Refs #1

---
+++ frontend/src/components/Assignment.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let assignmentInfo;
+  let solution = ""
+  const submitSolution = () => {
+    console.log(`helloo ${solution}`)
+  }
+</script>
+
+<li>
+  {assignmentInfo.title}
+</li>
+<input type="text" bind:value={solution} />
+<button on:click={submitSolution}>Submit solution</button>
---
+++ frontend/src/components/AssignmentList.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Assignment from './Assignment.svelte'
+
+  const getAssignments = async () => {
+    const response = await fetch("/api/assignments")
+    return await response.json()
+  }
+
+  let assignmentsPromise = getAssignments()
+</script>
+
+<h1>Assignments</h1>
+
+{#await assignmentsPromise}
+  <p>Loading assignments</p>
+{:then assignments}
+  {#if assignments.length == 0}
+    <p>No assignments available</p>
+  {:else}
+    <ul>
+    {#each assignments as assignment}
+      <Assignment assignmentInfo={assignment} />
+    {/each}
+    </ul>
+  {/if}
+{/await}
