0f721b0245f590398e703149b55a7b347c495afc
tikibeni <benjamin.blinnikka@hotmail.com>
2024-01-24 15:37:18 +0200

    Update assignment step-by-step logic on the UI
    However the localStorage value does not seem to update long-term
    as the progress is reset every time the page is refreshed. This
    needs to be fixed later when there is more time.
    
    Closes #4
    Refs. #5

--- backend/app.js
+++ backend/app.js
@@ -86,7 +86,10 @@ const handlePostSubmissions = async (request) => {
     // Give submission service the new info package
     await submissionService.addSubmission(submission)
 
-    return new Response(JSON.stringify({ result: submission.grader_feedback, correct: submission.isCorrect }))
+    return new Response(JSON.stringify({ 
+      result: submission.grader_feedback,
+      correct: submission.correct
+    }))
   }
   return new Response("Bad request", { status: 400 })
 }
--- frontend/src/components/Assignment.svelte
+++ frontend/src/components/Assignment.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { userUuid } from "../stores/stores.js";
+  import { userUuid, userStep } from "../stores/stores.js";
   
   export let assignmentInfo;
   let submission = ""
@@ -22,23 +22,39 @@
 
     const jsonResponse = await response.json();
     result = jsonResponse.result
-
-    // TODO: Increment store score by one when getting right first time
     // TODO: Split result string into pieces by matching:
     // "Traceback", "File", "NameError" and other errors
+
+    // Increment step by one if submission is correct
+    if (jsonResponse.correct && assignmentInfo.assignment_order == $userStep) {
+      const updatedStep = parseInt($userStep, 10) + 1
+      // TODO: Fix localStorage's update problem. Doesn't seem to update value to the localStorage.
+      userStep.set(updatedStep.toString())
+    }
+  }
+
+  const assignmentDisabled = () => {
+    return assignmentInfo.assignment_order > $userStep
   }
 </script>
 
 <li style="padding: 50px">
   <p style="font-size: 20px; font-weight: bold;">{assignmentInfo.title} </p><br/>
   <label for="submission-box-{assignmentInfo.id}" class="block mb-2 text-sm font-medium">{assignmentInfo.handout}</label>
-  <textarea 
-    id="submission-box-{assignmentInfo.id}" 
-    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-    bind:value={submission} 
-    placeholder="Enter the solution here" 
+  <textarea
+    id="submission-box-{assignmentInfo.id}"
+    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    placeholder="Enter the solution here"
+    disabled={assignmentDisabled()}
     rows="4"
+    bind:value={submission} 
   />
   <p class="block mb-2 text-sm font-medium">{result}</p>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded m-4" on:click={submit}>Submit solution</button>  
+  <button 
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded m-4"
+    disabled={assignmentDisabled()}
+    on:click={submit}
+  >
+    Submit solution
+  </button>  
 </li>
